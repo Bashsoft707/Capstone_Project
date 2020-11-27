c756a85788a82ef7de22bd141e27405e6e9187c9
Bashsoft707 <alausabashir@gmail.com>
2020-11-27 22:52:03 +0100

    adding functionalities

--- script.js
+++ script.js
@@ -1,77 +1,124 @@
-window.load=doShowAll();
+// window.load=doShowAll();
 
 const imageBox = document.querySelector(".image-box");
-let addBtn = document.querySelectorAll('.btn-primary');
+const buttons = document.querySelectorAll(".btn");
+let itemCount = document.querySelector('.item-count');
+let totalAmount = document.querySelector('#total');
+const cartList = document.querySelector("#cart-list");
 
-imageBox.addEventListener("onmouseover", function (){
-	addBtn.style.display = "block";
+const images = [
+	{
+		id: 1,
+		image: "https://i.ibb.co/ZYW3VTp/brown-brim.png"
+	},
+	{
+		id: 2,
+		image: "https://i.ibb.co/ypkgK0X/blue-beanie.png"
+	},
+	{
+		id: 3,
+		image: "https://i.ibb.co/QdJwgmp/brown-cowboy.png"
+	},
+	{
+		id: 4,
+		image: "https://i.ibb.co/RjBLWxB/grey-brim.png"
+	},
+	{
+		id: 5,
+		image: "https://i.ibb.co/0s3pdnc/adidas-nmd.png"
+	},
+	{
+		id: 6,
+		image: "https://i.ibb.co/dJbG1cT/yeezy.png"
+	},
+	{
+		id: 7,
+		image: "https://i.ibb.co/bPmVXyP/black-converse.png"
+	},
+	{
+		id: 8,
+		image: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png"
+	},
+]
+
+let total = 0; 
+
+buttons.forEach(function(button){
+	button.addEventListener("click", function () {
+
+      	var counter = 0;
+      	var setIntervalId = setInterval(function () {
+          counter += 1;
+      	}, 1000)
+
+ 		itemCount.textContent = setIntervalId;
+
+ 		let prc = parseInt(button.value);
+		total += prc;
+
+		totalAmount.textContent = total;
+
+		itemAdd(button, prc);
+	});
 });
 
-localStorage.myProperty="myValue";
 
-localStorage.setItem('propertyName','value');
-localStorage.getItem('propertyName');
-localStorage.removeItem('propertyName');
 
-const SaveItem = () => {
 
-    var name = document.section.products.image.value;
-    var name = document.section.products.pname.value;
-    var name = document.section.products.price.value;
-    localStorage.setItem(name, pname, price);
-    doShowAll();
+function itemAdd(button, prc){
 
+	let obj1 = {
+		prd: document.createElement("div"),
+		dsc: document.createElement("div"),
+		qty: document.createElement("div"),
+		price: document.createElement("div"),
+		rmv : document.createElement("div")
+	};
+
+	let { prd, dsc, qty, price, rmv } = obj1;
+	localStorage.setItem(obj1, 'id');
+
+	prd.setAttribute('class', '.col-md-3');
+	dsc.setAttribute('class', '.col-md-3');
+	qty.setAttribute('class', '.col-md-2');
+	price.setAttribute('class', '.col-md-2');
+	rmv.setAttribute('class', '.col-md-2');
+
+	let leftA = document.createTextNode("<");
+	let txt = document.createTextNode("0");
+	let rightA = document.createTextNode(">");
+
+	qty.appendChild(rightA);
+	qty.appendChild(leftA);
+	qty.appendChild(txt);	
+
+	images.map((productImage, index) => {
+		prd.innerHTML = `<img src = "${productImage.image}" />`;
+	});
+
+	dsc.textContent = button.name;
+	qty.textContent = leftA + txt + rightA;
+	price.textContent = prc;
+	rmv.textContent = "X";
+
+	cartList.appendChild(prd);
+	cartList.appendChild(dsc);
+	cartList.appendChild(qty);
+	cartList.appendChild(price);
+	cartList.appendChild(rmv);
+
+console.log("is working")
+	
+  	rmv.onclick = function() {
+    	total = total - prc;
+    	console.log(total);
+    	cartList.removeChild(prd);
+    	cartList.removeChild(dsc);
+		cartList.removeChild(qty);
+		cartList.removeChild(price);
+		cartList.removeChild(rmv);
+		totalAmount.textContent = total;
+  	}
+	
 }
 
-function doShowAll() {
-	if (CheckBrowser()) {
-		var key = "";
-		var list = "<tr><th>Image</th><th>Name</th></tr><tr><th>Price</th>\n";
-		var i = 0;
-		//for more advance feature, you can set cap on max items in the cart
-		for (i = 0; i <= localStorage.length-1; i++) {
-			key = localStorage.key(i);
-			list += "<tr><td>" + key + "</td>\n<td>"
-					+ localStorage.getItem(key) + "</td></tr>\n";
-		}
-		//if no item exists in the cart
-		if (list == "<tr><th>Image</th><th>Name</th></tr><tr><th>Price</th>\n") {
-			list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-		}
-		//bind the data to html table
-		//you can use jQuery too....
-		document.getElementById('list').innerHTML = list;
-	} else {
-		alert('Cannot save shopping list as your browser does not support HTML 5');
-	}
-}
-
-function CheckBrowser() {
-	if ('localStorage' in window && window['localStorage'] !== null) {
-		// we can use localStorage object to store data
-		return true;
-	} else {
-			return false;
-	}
-}
-
-// let addBtn = document.querySelectorAll('.btn-primary');
-// let totalAmount = document.querySelector('#total');
-
-// var total = 0;
-
-// for (let wears of addBtn) {
-// 	wears.addEventListener('click', () => {
-		
-// 		let prc = parseInt(wears.value);
-// 		total += prc;
-
-// 		console.log(`total bill ${total}`);
-
-// 		// total amount added to total after cart if any item is added
-// 		totalAmount.textContent = total;
-
-// 		// seperate function made for
-// 		itemAdd(wears, prc);
-// 	});
-// }
